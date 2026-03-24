8c70d2c243f670a0b535a5376c07314a1cdc4bde
balaji1832 <balaji49034@gmail.com>
2026-03-24 15:15:29 +0530

    Mobile hanburger menu done

--- js/hamburger-script.js
+++ js/hamburger-script.js
@@ -1,8 +1,13 @@
-//  Hamburger script 
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
 
-      const menuBtn = document.getElementById("menuBtn");
-      const mobileMenu = document.getElementById("mobileMenu");
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  mobileMenu.classList.toggle("hidden");
+});
 
-      menuBtn.addEventListener("click", () => {
-        mobileMenu.classList.toggle("hidden");
-      });
+document.addEventListener("click", (e) => {
+  if (!mobileMenu.contains(e.target) && !menuBtn.contains(e.target)) {
+    mobileMenu.classList.add("hidden");
+  }
+});
